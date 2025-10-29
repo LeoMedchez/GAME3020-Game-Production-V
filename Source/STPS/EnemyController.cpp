f42dbdb47eb4538ee25eee30059f7d72a9adc37d
LeoMedchez <151944849+LeoMedchez@users.noreply.github.com>
2025-10-29 12:35:25 -0600

    Enemy Detects By Sight & Sound
    ~Added Enemy Character (No Longer Shares PC Asset)
    ~Added Takedown Animations
    ~Created C++ For Player Character Identification (Blueprint Reparented To Custom Class)

--- Source/STPS/EnemyController.cpp
+++ Source/STPS/EnemyController.cpp
@@ -5,14 +5,51 @@
 #include "EnemyCharacter.h"
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISense_Sight.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISense_Hearing.h"
+#include "Perception/AISenseConfig_Hearing.h"
+#include <Kismet/GameplayStatics.h>
+#include "MainCharacter.h"
 
 AEnemyController::AEnemyController()
+	:Super()
 {
 	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>(TEXT("BehaviorTreeComp"));
 	check(BehaviorTreeComponent);
 
 	BlackboardComponent = CreateDefaultSubobject<UBlackboardComponent>(TEXT("BlackboardComp"));
 	check(BlackboardComponent);
+
+	PerceptionComponent = CreateDefaultSubobject <UAIPerceptionComponent>(TEXT("PerceptionComp"));
+
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(FName("Sight Config"));
+	HearingConfig = CreateDefaultSubobject<UAISenseConfig_Hearing>(FName("Hearing Config"));
+
+	SightConfig->SightRadius = 700.0f;
+	SightConfig->LoseSightRadius = 900.0f;
+	SightConfig->PeripheralVisionAngleDegrees = 45;
+	SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+	SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+
+	HearingConfig->HearingRange = 900.0f;
+	HearingConfig->SetMaxAge(2);
+	HearingConfig->DetectionByAffiliation.bDetectEnemies = true;
+	HearingConfig->DetectionByAffiliation.bDetectNeutrals = true;
+	HearingConfig->DetectionByAffiliation.bDetectFriendlies = true;
+
+	PerceptionComponent->ConfigureSense(*SightConfig);
+	PerceptionComponent->ConfigureSense(*HearingConfig);
+	PerceptionComponent->SetDominantSense(SightConfig->GetSenseImplementation());
+}
+
+void AEnemyController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AEnemyController::OnTargetPerceptionUpdated);
 }
 
 void AEnemyController::OnPossess(APawn* InPawn)
@@ -34,5 +71,54 @@ void AEnemyController::OnPossess(APawn* InPawn)
 			}
 			BehaviorTreeComponent->StartTree(*(Enemy->BehaviorTree));
 		}
+	}
+}
+
+void AEnemyController::OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	if (Actor == nullptr) return;
+
+	if (AMainCharacter* PlayerRef = Cast<AMainCharacter>(Actor))
+	{
+		if (Stimulus.Type == SightConfig->GetSenseID())
+		{
+			if (Stimulus.WasSuccessfullySensed())
+			{
+				bCanSeePlayer = true;
+				BlackboardComponent->SetValueAsBool("IsInvestigating", bCanSeePlayer);
+			}
+			else
+			{
+				bCanSeePlayer = false;
+				BlackboardComponent->SetValueAsBool("IsInvestigating", bCanSeePlayer);
+			}
+		}
+
+		if (Stimulus.Type == HearingConfig->GetSenseID())
+		{
+			if (Stimulus.WasSuccessfullySensed())
+			{
+				bHeardSound = true;
+			}
+			else
+			{
+				bHeardSound = false;
+			}
+		}
+	}
+}
+
+void AEnemyController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (bCanSeePlayer)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 0.1f, FColor::Red, TEXT("Player Seen"));
+	}
+
+	if (bHeardSound)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 0.1f, FColor::Red, TEXT("Player Heard"));
 	}
 }
--- Source/STPS/EnemyController.h
+++ Source/STPS/EnemyController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include <Perception/AIPerceptionTypes.h>
 #include "EnemyController.generated.h"
 
 /**
@@ -28,4 +29,29 @@ protected:
 	
 	UPROPERTY(BlueprintReadWrite, Category = Behavior)
 	TObjectPtr<class UBlackboardComponent> BlackboardComponent;
+
+public:
+	UFUNCTION()
+	void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	
+	UPROPERTY(VisibleAnywhere, Category = "AI")
+	bool bCanSeePlayer;
+
+	UPROPERTY(VisibleAnywhere, Category = "AI")
+	bool bHeardSound;
+
+protected:
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "AI")
+	TObjectPtr<class UAISenseConfig_Sight> SightConfig;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "AI")
+	TObjectPtr<class UAISenseConfig_Hearing> HearingConfig;
 };
